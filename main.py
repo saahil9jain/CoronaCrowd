286667b7b4917e6ccc02ba0ef6b542cb74e05556
Gyan Prayaga <reachgyan@gmail.com>
2020-03-29 16:25:40 -0700

    adds some (broken) firebase logic in python

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pyrebase # for connecting to firebase
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,8 +8,26 @@ def home():
 
 @app.route('/crowd/<crowd_id>')
 def crowd(crowd_id=None):
-    crowd_obj = {
-        'id' : crowd_id,
-        'name': 'Dunkin donuts'
+
+    # config firebase
+    config = {
+      "apiKey": "AIzaSyA-TtymaKaQYS1LiedsCmgdj3kV5Z2TtOo",
+      "authDomain": "corona-crowd-cf195.firebaseapp.com",
+      "databaseURL": "https://corona-crowd-cf195.firebaseio.com",
+      "storageBucket": "corona-crowd-cf195.appspot.com"
     }
-    return render_template('crowd.html', crowd=crowd_obj)
+    firebase = pyrebase.initialize_app(config)
+
+    # Get a reference to the database service
+    db = firebase.database()
+
+    matched_crowd = db.child("crowds").order_by_key().equal_to(crowd_id).get()
+    crowd_obj = matched_crowd.val()
+
+    # crowd_obj = {
+    #     'id' : crowd_id,
+    #     'name': 'Dunkin donuts'
+    # }
+
+    return crowd_obj
+    # return render_template('crowd.html', crowd=crowd_obj)
